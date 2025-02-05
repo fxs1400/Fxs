5ed4e8fc92f2334787f391fbe7197676e2debec7
fxs1400 <unufx30@gmail.com>
2025-02-05 12:38:45 +0100

    Hhhh.c

---
+++ Hhhh.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main (){
+printf("hello in scw \n 1 Calculator \n 2 Writing numbers letters");
+int g;
+  scanf("%d", &g);
+  if (g = 1) {
+    int f;
+    int x;
+    scanf("%d", &f);
+    scanf("%d", &x);
+    printf("%d + %d", f, x);
+  }
+else if (g = 2) {
+  int r;
+  scanf("%d", &r);
+  printf("%c", r);
+}
+
+
+  
+}
